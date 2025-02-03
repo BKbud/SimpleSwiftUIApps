587cd8a00da1ff621305541d30dfdef6b80c0a84
BKbud <tsb07040@gmail.com>
2025-02-03 15:55:36 +0900

    build frame with: NavigationStack, Section, navigationLink, Dealing with Image

--- SimpleSettingApp/SimpleSettingApp/ContentView.swift
+++ SimpleSettingApp/SimpleSettingApp/ContentView.swift
@@ -9,13 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    NavigationLink {
+                        Text("Screen Person")
+                    } label: {
+                        HStack {
+                            Image(systemName: "person")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .padding(5)
+                                .background(.black)
+                                .foregroundStyle(.white)
+                                .clipShape(.circle)
+                                .padding(.all, 5)
+                            VStack(alignment: .leading, spacing: 3) {
+                                Text("Name")
+                                    .font(.system(size: 24))
+                                    .fontWeight(.regular)
+                                Text("description")
+                                    .font(.system(size: 14))
+                            }
+                            .padding(.leading, 5)
+                        }
+                        .padding(.vertical, 5)
+                    }
+                }
+                Section {
+                    NavigationLink(destination: Text("Screen A")) {
+                        HStack {
+                            Image(systemName: "folder")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .padding(5)
+                                .background(.indigo)
+                                .foregroundStyle(.white)
+                                .cornerRadius(5)
+                            Text("A")
+                        }
+                    }
+                    NavigationLink(destination: Text("Screen B")) {
+                        HStack {
+                            Image(systemName: "person")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .padding(5)
+                                .background(.green)
+                                .foregroundStyle(.white)
+                                .cornerRadius(5)
+                            Text("B")
+                        }
+                    }
+                }
+                NavigationLink(destination: Text("Screen C")) {
+                    HStack {
+                        Image(systemName: "calendar")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .padding(5)
+                            .background(.blue)
+                            .foregroundStyle(.white)
+                            .cornerRadius(5)
+                        Text("C")
+                    }
+                }
+                NavigationLink(destination: Text("Screen D")) {
+                    HStack {
+                        Image(systemName: "airplane")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .padding(5)
+                            .background(.purple)
+                            .foregroundStyle(.white)
+                            .cornerRadius(5)
+                        Text("D")
+                    }
+                }
+            }
+            .navigationTitle("Setting")
         }
-        .padding()
     }
 }
 
