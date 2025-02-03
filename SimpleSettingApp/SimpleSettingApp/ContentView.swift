99cde6d9ee909fcd15e4692ff40bc61dd8238600
BKbud <tsb07040@gmail.com>
2025-02-03 14:30:32 +0900

    initial commit

---
+++ SimpleSettingApp/SimpleSettingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleSettingApp
+//
+//  Created by 정보경 on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleSettingApp/SimpleSettingApp/SimpleSettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSettingAppApp.swift
+//  SimpleSettingApp
+//
+//  Created by 정보경 on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
