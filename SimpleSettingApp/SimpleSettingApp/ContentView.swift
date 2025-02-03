c67155e970edccf3c38f98982a37fc991629a306
BKbud <tsb07040@gmail.com>
2025-02-03 22:32:52 +0900

    Refactoring NavigationCellView, imageView, toggleView, profileView

--- SimpleSettingApp/SimpleSettingApp/ContentView.swift
+++ SimpleSettingApp/SimpleSettingApp/ContentView.swift
@@ -18,132 +18,125 @@ struct ContentView: View {
                     NavigationLink {
                         Text("Screen Person")
                     } label: {
-                        HStack {
-                            Image(systemName: "person")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 30, height: 30)
-                                .padding(5)
-                                .background(.black)
-                                .foregroundStyle(.white)
-                                .clipShape(.circle)
-                                .padding(.all, 5)
-                            VStack(alignment: .leading, spacing: 3) {
-                                Text("Name")
-                                    .font(.system(size: 24))
-                                    .fontWeight(.regular)
-                                Text("description")
-                                    .font(.system(size: 14))
-                            }
-                            .padding(.leading, 5)
-                        }
-                        .padding(.vertical, 5)
+                        profileCell()
                     }
                 }
                 Section {
-                    NavigationLink(destination: Text("Screen A")) {
-                        HStack {
-                            Image(systemName: "folder")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                                .background(.indigo)
-                                .foregroundStyle(.white)
-                                .cornerRadius(5)
-                            Text("A")
-                        }
-                    }
-                    NavigationLink(destination: Text("Screen B")) {
-                        HStack {
-                            Image(systemName: "person")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                                .background(.green)
-                                .foregroundStyle(.white)
-                                .cornerRadius(5)
-                            Text("B")
-                        }
-                    }
+                    
+                    plainCell(imageName: "folder",
+                              imageColor: .indigo,
+                              cellTitle: "Folder",
+                              destination: {
+                        Text("Folder Screen")
+                    })
+                    plainCell(imageName: "person",
+                              imageColor: .green,
+                              cellTitle: "Contact",
+                              destination: {
+                        Text("Contact Screen")
+                    })
                 }
                 Section {
-                    NavigationLink(destination: Text("Screen C")) {
-                        HStack {
-                            Image(systemName: "calendar")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                                .background(.blue)
-                                .foregroundStyle(.white)
-                                .cornerRadius(5)
-                            Text("C")
-                        }
-                    }
+                    plainCell(imageName: "calendar",
+                              imageColor: .blue,
+                              cellTitle: "Calendar",
+                              destination: {
+                        Text("Calendar Screen")
+                    })
                 }
                 Section {
-                    HStack {
-                        Image(systemName: "airplane")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(5)
-                            .background(.purple)
-                            .foregroundStyle(.white)
-                            .cornerRadius(5)
-                        Text("Airplane mode")
-                        Spacer()
-                        Toggle(isOn: $airplaneSettingOn) {
-                            
-                        }
+                    toggleCell(imageName: "airplane",
+                               imageColor: .blue,
+                               cellTitle: "Airplane mode",
+                               isOn: $airplaneSettingOn)
+                    
+                    plainCell(imageName: "wifi",
+                              imageColor: .blue,
+                              cellTitle: "Wi-Fi",
+                              subTitle: "Wi-Fi Description") {
+                        Text("Wi-Fi Screen")
                     }
-                    NavigationLink(destination: Text("Screen wifi")) {
-                        HStack {
-                            Image(systemName: "wifi")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                                .background(.blue)
-                                .foregroundStyle(.white)
-                                .cornerRadius(5)
-                            Text("Wi-Fi")
-                            Spacer()
-                            Text("Wi-Fi Description")
-                                .foregroundStyle(.gray)
-                        }
+                    
+                    plainCell(imageName: "bolt", imageColor: .blue, cellTitle: "Bluetooth", subTitle: "myBluetooth") {
+                        Text("Bluetooth Screen")
                     }
-                    NavigationLink(destination: Text("Screen E")) {
-                        HStack {
-                            Image(systemName: "figure.wave.circle")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                                .background(.orange)
-                                .foregroundStyle(.white)
-                                .cornerRadius(5)
-                            Text("E")
-                        }
-                    }
-                    NavigationLink(destination: Text("Screen Setting")) {
-                        HStack {
-                            Image(systemName: "gearshape.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 20, height: 20)
-                                .padding(5)
-                                .background(.gray)
-                                .foregroundStyle(.white)
-                                .cornerRadius(5)
-                            Text("Setting")
-                        }
-                    }
+                    
+                    plainCell(imageName: "figure.wave.circle",
+                              imageColor: .orange,
+                              cellTitle: "Shortcut",
+                              destination: {
+                        Text("Shortcut Screen")
+                    })
+                    plainCell(imageName: "gearshape.fill",
+                              imageColor: .gray,
+                              cellTitle: "Setting",
+                              destination: {
+                        Text("Setting Screen")
+                    })
                 }
             }
             .navigationTitle("Setting")
+        }
+    }
+    
+    private func profileCell() -> some View {
+        HStack {
+            Image(systemName: "person")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+                .padding(8)
+                .background(Color.teal)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .padding(.all, 5)
+            VStack(alignment: .leading, spacing: 3) {
+                Text("Name")
+                    .font(.system(size: 24))
+                    .fontWeight(.regular)
+                Text("description")
+                    .font(.system(size: 14))
+            }
+            .padding(.leading, 5)
+        }
+        .padding(.vertical, 5)
+    }
+    
+    private func imageView(imageName: String, imageColor: Color) -> some View {
+        Image(systemName: imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 20, height: 20)
+            .padding(5)
+            .background(imageColor)
+            .foregroundStyle(.white)
+            .cornerRadius(5)
+    }
+
+    private func toggleCell(imageName: String, imageColor: Color, cellTitle: String, isOn: Binding<Bool>) -> some View {
+        HStack {
+            imageView(imageName: imageName, imageColor: imageColor)
+            Text(cellTitle)
+            Spacer()
+            Toggle(isOn: isOn) {
+                // toggle action
+            }
+        }
+    }
+
+    //@ViewBuilder // 사용자가 클로저에 복수의 View를 선언할 수 있도록 지원
+    private func plainCell<V: View>(imageName: String, imageColor: Color, cellTitle: String, subTitle: String? = nil, destination: @escaping () -> V) -> some View {
+        NavigationLink(destination: destination()) {
+            HStack {
+                imageView(imageName: imageName, imageColor: imageColor)
+                Text(cellTitle)
+                
+                if let subTitle = subTitle {
+                    Spacer()
+                    Text(subTitle)
+                        .foregroundStyle(.gray)
+                }
+            }
         }
     }
 }
